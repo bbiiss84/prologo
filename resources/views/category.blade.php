10dac920f53af2351cdedc72fbe66321b7789724
bbiiss <bbiiss@mail.ru>
2023-02-21 16:46:06 +0300

    Карточка товара

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -10,21 +10,9 @@
             {{ $category->description }}
         </p>
         <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <img src="http://laravel-diplom-1.rdavydov.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
-                    <div class="caption">
-                        <h3>iPhone X 64GB</h3>
-                        <p>71990 руб.</p>
-                        <p>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/basket/1/add" class="btn btn-primary"
-                                role="button">В корзину</a>
-                            <a href="http://laravel-diplom-1.rdavydov.ru/mobiles/iphone_x_64" class="btn btn-default"
-                                role="button">Подробнее</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <x-card>
+
+            </x-card>
             <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
                     <img src="http://laravel-diplom-1.rdavydov.ru/storage/products/iphone_x_silver.jpg"
---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,19 @@
+<div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+        <div class="labels">
+        </div>
+        <img src="/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
+        <div class="caption">
+            <h3>iPhone X 64GB</h3>
+            <p>71990 ₽</p>
+            <p>
+            </p>
+            <form action="/basket/add/1" method="POST">
+                <button type="submit" class="btn btn-primary" role="button">В корзину</button>
+                <a href="/mobiles/iphone_x_64" class="btn btn-default" role="button">Подробнее</a>
+                <input type="hidden" name="_token">
+            </form>
+            <p></p>
+        </div>
+    </div>
+</div>
