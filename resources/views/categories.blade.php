86aa742fa614feb097a3afda3fc612e39c143527
bbiiss <bbiiss@mail.ru>
2023-02-21 16:42:34 +0300

    Мастер шаблон

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,86 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Интернет Магазин: Все Категории</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/starter-template.css" rel="stylesheet">
-    <script src="chrome-extension://mooikfkahbdckldjjndioackbalphokd/assets/prompt.js"></script>
-</head>
-
-<body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="/">Интернет Магазин</a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="/">Все товары</a></li>
-                    <li class="active"><a href="/categories">Категории</a>
-                    </li>
-                    <li><a href="/basket">В корзину</a></li>
-                    <li><a href="/reset">Сбросить проект в начальное состояние</a></li>
-                    <li><a href="/locale/en">en</a></li>
-
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false">₽<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="/currency/RUB">₽</a></li>
-                            <li><a href="/currency/USD">$</a></li>
-                            <li><a href="/currency/EUR">€</a></li>
-                        </ul>
-                    </li>
-                </ul>
-
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/login">Войти</a></li>
-
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container">
-        <div class="starter-template">
-            @foreach ($categories as $category)
-                <div class="panel">
-                    <a href="/{{ $category->code }}">
-                        <img src="/storage/categories/mobile.jpg">
-                        <h2>{{ $category->name }}</h2>
-                    </a>
-                    <p>
-                        {{ $category->description }}
-                    </p>
-                </div>
-            @endforeach
+<x-layout>
+    <x-slot name='title'>
+        Категории
+    </x-slot>
+    <div class="starter-template">
+        @foreach ($categories as $category)
             <div class="panel">
-                <a href="/portable">
-                    <img src="/storage/categories/portable.jpg">
-                    <h2>Портативная техника</h2>
+                <a href="/{{ $category->code }}">
+                    <img src="/storage/categories/mobile.jpg">
+                    <h2>{{ $category->name }}</h2>
                 </a>
                 <p>
-                    Раздел с портативной техникой.
+                    {{ $category->description }}
                 </p>
             </div>
-            <div class="panel">
-                <a href="/appliances">
-                    <img src="/storage/categories/appliance.jpg">
-                    <h2>Бытовая техника</h2>
-                </a>
-                <p>
-                    Раздел с бытовой техникой
-                </p>
-            </div>
-        </div>
+        @endforeach
     </div>
-</body>
+</x-layout>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,70 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+    <x-slot name='title'>
+        Товар
+    </x-slot>
+    <div class="starter-template">
+        <h1>iPhone X 64GB</h1>
+        <h2>Мобильные телефоны</h2>
+        <p>Цена: <b>71990 ₽</b></p>
+        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg">
+        <p>Отличный продвинутый телефон с памятью на 64 gb</p>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
+            <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
 
-    <title>Интернет Магазин: Товар</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/starter-template.css" rel="stylesheet">
-    <script src="chrome-extension://mooikfkahbdckldjjndioackbalphokd/assets/prompt.js"></script>
-</head>
-
-<body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="http://internet-shop.tmweb.ru">Интернет Магазин</a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="http://internet-shop.tmweb.ru">Все товары</a></li>
-                    <li><a href="http://internet-shop.tmweb.ru/categories">Категории</a>
-                    </li>
-                    <li><a href="http://internet-shop.tmweb.ru/basket">В корзину</a></li>
-                    <li><a href="http://internet-shop.tmweb.ru/reset">Сбросить проект в начальное состояние</a></li>
-                    <li><a href="http://internet-shop.tmweb.ru/locale/en">en</a></li>
-
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false">₽<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="http://internet-shop.tmweb.ru/currency/RUB">₽</a></li>
-                            <li><a href="http://internet-shop.tmweb.ru/currency/USD">$</a></li>
-                            <li><a href="http://internet-shop.tmweb.ru/currency/EUR">€</a></li>
-                        </ul>
-                    </li>
-                </ul>
-
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="http://internet-shop.tmweb.ru/login">Войти</a></li>
-
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container">
-        <div class="starter-template">
-            <h1>iPhone X 64GB</h1>
-            <h2>Мобильные телефоны</h2>
-            <p>Цена: <b>71990 ₽</b></p>
-            <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg">
-            <p>Отличный продвинутый телефон с памятью на 64 gb</p>
-
-            <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
-                <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
-
-                <input type="hidden" name="_token" value="BbjfW9DrT4VMnbs5XSilm73s8ukmMStKoleyRyqO">
-            </form>
-        </div>
+            <input type="hidden" name="_token" value="BbjfW9DrT4VMnbs5XSilm73s8ukmMStKoleyRyqO">
+        </form>
     </div>
-</body>
-
-</html>
+</x-layout>
