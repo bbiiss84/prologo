60a3972a90fc229b0485761d99976bd1abeb6709
bbiiss <bbiiss@mail.ru>
2023-02-21 16:47:32 +0300

    Добавили маршрутам имена

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,11 @@ require __DIR__.'/auth.php';
 
 /*============================*/
 
-Route::get('/', [MainController::class, 'index']);
+Route::get('/', [MainController::class, 'index'])->name('index');
 
-Route::get('/categories', [MainController::class, 'categories']);
+Route::get('/categories', [MainController::class, 'categories'])->name('categories');
 
-Route::get('/{category}', [MainController::class, 'category']);
+Route::get('/{category}', [MainController::class, 'category'])->name('category');
 
-Route::get('/mobiles/{product?}', [MainController::class, 'product']);
+Route::get('/{category}/{product?}', [MainController::class, 'product'])->name('product');
 
